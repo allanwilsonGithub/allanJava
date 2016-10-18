08f3180aa0f2fc313a174f8640b807f59aece87a
Allan Wilson <allan@allanwilson.net>
2016-10-18 23:50:13 +0300

    variables in Java

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,13 @@
 /**
- * Created by allu on 10/11/16.
+ * Created by allan on 10/11/16.
  */
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("Hello Allan!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
+        System.out.println("myFirstNumber");
     }
 }
