cd02afa74e0adb73ab9f090a406d97ac97c01641
Allan Wilson <allan.wilson@vaisala.com>
2017-01-23 13:03:28 +0200

    variables

---
+++ src/variables.java
@@ -0,0 +1,14 @@
+/**
+ * Created by awil on 2017-01-23.
+ */
+public class variables {
+
+    public static void main(String[] args) {
+
+        // int has a width of 32
+        int myMinValue= -2_147_483_648;
+        int myMaxValue= 2_147_483_647;
+        System.out.println(myMinValue);
+        System.out.println(myMaxValue);
+    }
+}
